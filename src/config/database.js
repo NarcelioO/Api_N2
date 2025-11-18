d4c3927af151f6625bfe7a69bed2a6b23f696479
Narcelio Oliveira <48735391+NarcelioO@users.noreply.github.com>
2025-11-18 14:19:35 -0300

    Change MongoDB connection string in database.js
    Updated MongoDB connection string to use a specific URI.

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export async function connectDB(){
     try{
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect("mongodb+srv://narcelioferffgh_db_user:CCiQZcNvH4o7WV0v@cluster0.icf3b3c.mongodb.net/?appName=Cluster0");
         console.log('Conectado ao banco de dados MongoDB');
     }catch(error){
         console.error('Erro ao conectar ao banco de dados:', error);
